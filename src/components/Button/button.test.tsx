ea9845cd0d61a2215068c83ccf71b6652eb83d85
DAHUO-Melbourne <huodamelbourne@gmail.com>
2023-01-19 02:45:11 +1100

    feat: 5-4

--- src/components/Button/button.test.tsx
+++ src/components/Button/button.test.tsx
@@ -6,7 +6,22 @@ test('our first react test case', () => {
   render(<Button>Nice</Button>);
   const element = screen.queryByText('Nice');
   expect(element).toBeTruthy();
+  expect(element).toBeInTheDocument();
 });
+
+describe('test Button component', () => {
+  it('should render the correct default button', () => {
+    render(<Button>Nice</Button>);
+    const element = screen.getByText('Nice');
+    expect(element).toBeInTheDocument();
+    expect(element.tagName).toEqual('BUTTON');
+    expect(element.tagName).toHaveClass('btn btn-default');
+  });
+  // 测试默认的button的render出来的type需要是一个button，类名需要是btn btn-default
+  it('should render the correct component based on different props', () => {});
+  it('should render a link when btnType equals link and href props', () => {});
+  it('should render disable button when disabled set to be true', () => {});
+})
 // 首先在屏幕上render组件
 // 然后期待组件的text和我们写的组件的children一样
 // 最后期待这个断言，是true
