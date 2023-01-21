ed71640233e1f3284a57eec9e78ba457713c8161
DAHUO-Melbourne <huodamelbourne@gmail.com>
2023-01-21 19:50:40 +1100

    5-5 full

--- src/components/Button/button.test.tsx
+++ src/components/Button/button.test.tsx
@@ -11,6 +11,12 @@ const disabledProps: ButtonProps = {
   onClick: jest.fn(),
 }
 
+const testProps: ButtonProps = {
+  btnType: ButtonType.Primary,
+  size: ButtonSize.Large,
+  className: 'klass',
+}
+
 test('our first react test case', () => {
   render(<Button>Nice</Button>);
   const element = screen.queryByText('Nice');
@@ -31,11 +37,28 @@ describe('test Button component', () => {
   // 测试默认的button的render出来的type需要是一个button，类名需要是btn btn-default
   // 并且用模拟的fireEvent来模拟用户事件。
   // 通过展开defaultProps来为组件添加props
-  it('should render the correct component based on different props', () => {});
-  it('should render a link when btnType equals link and href props', () => {});
+  it('should render the correct component based on different props', () => {
+    render(<Button {...testProps}>Nice</Button>);
+    // 通过改变button的props的值从而修改测试的button的类型，这样render出来的就是你需要测试的类型: 比如说Primary + Large
+    const element = screen.getByText('Nice');
+    expect(element).toBeInTheDocument();
+    expect(element).toHaveClass('btn-primary btn-lg klass');
+    // 如果primary && large类型的button成功render，那么它的className就应该是btn-primary btn-lg，且应该还有传入的参数className：klass
+
+  });
+  it('should render a link when btnType equals link and href props', () => {
+    render(<Button btnType={ButtonType.Link} href='http://dummyurl' >Link</Button>);
+    const element = screen.getByText('Link');
+    expect(element).toBeInTheDocument();
+    expect(element.tagName).toEqual('A');
+    expect(element).toHaveClass('btn btn-link');
+  });
   it('should render disable button when disabled set to be true', () => {
     render(<Button {...disabledProps}>Nice</Button>);
     const element = screen.getByText('Nice') as HTMLButtonElement;
+    // 类型断言：原来的element的类型是一个HTMLElement，但是HTMLElement不一定有disabled属性(见后两行)。为了拿到这个属性需要用到类型断言。
+    // 类型断言的意思就是，当A类型包含了B类型，也包含了C类型，D类型，是一个不同类型的集合。但是我们在使用的时候，需要让他指定为其中一个子类型
+    // 这个时候就可以用到as断言
     expect(element).toBeInTheDocument();
     expect(element.disabled).toBeTruthy();
     fireEvent.click(element);
