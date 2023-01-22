d989742e08f24ba97c52ac134339bc932c7b7c99
DAHUO-Melbourne <huodamelbourne@gmail.com>
2023-01-23 02:27:00 +1100

    feat: menu is added

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import './App.css';
 import Button, {ButtonType, ButtonSize} from './components/Button/button';
 import Menu from './components/Menu/menu';
 import MenuItem from './components/Menu/menuItem';
@@ -8,7 +7,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Menu defaultIndex={0} onSelect={(index) => alert(index)}>
+        <Menu defaultIndex={0} onSelect={(index) => alert(index)} mode='vertical'>
           <MenuItem index={0}>
             cool link 1
           </MenuItem>
