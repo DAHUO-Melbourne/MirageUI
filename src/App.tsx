60063895cd09cbc4778fb3a159bb10aba3da1f78
DAHUO-Melbourne <huodamelbourne@gmail.com>
2023-01-27 01:00:28 +1100

    feat: 7-1

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,14 @@ import Button, {ButtonType, ButtonSize} from './components/Button/button';
 import Menu from './components/Menu/menu';
 import MenuItem from './components/Menu/menuItem';
 import SubMenu from './components/Menu/subMenu';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCoffee } from '@fortawesome/free-solid-svg-icons'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <FontAwesomeIcon icon={faCoffee} size='lg' />
         <Menu defaultIndex='0' onSelect={(index) => alert(index)} mode='vertical' defaultOpenSubmenus={['3']}>
           <MenuItem>
             cool link 1
