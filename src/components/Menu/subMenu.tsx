db4d8a9115b8c6be9d7bb2bdcc02ae57d7936c46
DAHUO-Melbourne <huodamelbourne@gmail.com>
2023-01-29 00:48:28 +1100

    7-8

--- src/components/Menu/subMenu.tsx
+++ src/components/Menu/subMenu.tsx
@@ -4,6 +4,7 @@ import { MenuContext } from './menu';
 import {MenuItemProps} from './menuItem';
 import Icon from '../Icon/Icon';
 import { CSSTransition } from 'react-transition-group';
+import Transition from '../Transition/Transition';
 
 export interface SubMenuProps { 
   index?: string;
@@ -73,23 +74,22 @@ const SubMenu: React.FC<SubMenuProps> = ({
       }
     })
     return (
-      <CSSTransition
+      <Transition
         in={menuOpen}
         // menuOpen为true，则开始添加动画效果-enter路线。false则添加-exit路线
-        unmountOnExit
         // dropdown的显示和隐藏是靠类名来决定的，但是这样很影响className的复用。
         // 现在我们做个更改：通过一prop，来让他in为true则显示子元素，in为false则隐藏子元素
         // 这个prop的名字就是unmountOnExit，unmountOnExit默认为false，但是如果为true，意思就是当类名为*-exit-done，则隐藏子元素
         timeout={300}
         // 300ms以后开始进行动画过程(即添加enter(exit)等类)
         appear
-        classNames='zoom-in-top'
+        animation='zoom-in-bottom'
         // 使用zoom-in-top-enter; zoom-in-top-enter-active; zoom-in-top-exit; zoom-in-top-exit-active来添加动画。一般是在active上添加transition
       >
         <ul className={subMenuClasses}>
           {childrenComponent}
         </ul>
-      </CSSTransition>
+      </Transition>
     )
   }
 
