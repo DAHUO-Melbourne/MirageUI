810dfa374af8d3b338fb3e36e88161e1bca527bb
DAHUO-Melbourne <huodamelbourne@gmail.com>
2023-02-01 01:13:49 +1100

    8-6

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -6,10 +6,15 @@ import '../src/styles/index.scss';
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
+  // 所有以on开头的事件都会被自动记录，所以onSelect才会被记下来
   controls: {
     matchers: {
       color: /(background|color)$/i,
       date: /Date$/,
+      // color 和 date，都是storybook里控制输入类型的控件类型
+      // 比如说如果是color就意味着输入的值必须是hex value，如果是date就必须是date value
+      // 这里的语法意思是，如果你在代码里配置控件类型，只要你输入background/color，那么就会采用color 控件类型
+      // 如果输入Date，那么就会采用date控件类型
     },
   },
 }
--- src/components/Menu/Menu.stories.tsx
+++ src/components/Menu/Menu.stories.tsx
@@ -22,9 +22,23 @@ const menuMeta: ComponentMeta<typeof Menu> =  {
       description: 'normal text',
       // 出现在类型上有一行描述，来解释defaultIndex这个属性是干啥的，我们给description里设置的内容就是normal text，这个开发者爱写什么写什么
     }
-  }
+  },
   // 也可以在menuMeta上进行设置
+
+  parameters: {
+    controls: {
+      matchers: {
+        date: /mode$/,
+        // 我们希望将‘mode’属性设置为date类型的输入类型，于是如果想要修改mode的值，就只能输入date类型的值了
+      }
+    }
+  }
 }
+
+/**
+ * 插件的集合有：doc/control/actions/viewport/backgrounds/toolbars && globals/measure && outline
+ * https://storybook.js.org/docs/react/essentials/introduction
+ */
 
 export default menuMeta;
 
@@ -69,5 +83,14 @@ VerticalMenu.argTypes = {
 // 首先：这些属性的类型是storybook根据程序的逻辑自行推断出来的，难免会有一些失误的地方
 // 当我们觉得他文档中给出的类型有误，我们就可以使用argTypes来一个个设置不同属性的类型。具体的就是通过control属性来设置
 // https://storybook.js.org/docs/react/essentials/controls
+
+VerticalMenu.parameters = {
+  backgrounds: {
+    values: [
+      {name: 'red', value: '#f00'},
+      {name: 'green', value: '#0f0'}
+    ]
+  }
+}
 
 VerticalMenu.storyName='Vertical Menu'
