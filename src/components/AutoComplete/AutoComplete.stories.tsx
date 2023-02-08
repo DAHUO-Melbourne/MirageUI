d73b6e5d874a39ad8834409333843282ac2857fc
DAHUO-Melbourne <huodamelbourne@gmail.com>
2023-02-08 22:22:33 +1100

    second part of template

--- src/components/AutoComplete/AutoComplete.stories.tsx
+++ src/components/AutoComplete/AutoComplete.stories.tsx
@@ -2,17 +2,44 @@ import React from "react";
 import { storiesOf } from "@storybook/react";
 import {action} from '@storybook/addon-actions'
 import AutoComplete from "./AutoComplete";
+import {DataSourseType} from './AutoComplete';
+
+interface LakerPlayerProps {
+  value: string;
+  number: number;
+}
 
 const SimpleComplete = () => {
   const lakers = ['bradley', 'pope', 'caruso', 'cook', 'cousins',
-  'james', 'AD', 'green', 'howard', 'kuzma', 'McGee', 'rando']
+  'james', 'AD', 'green', 'howard', 'kuzma', 'McGee', 'rando'];
+
+  const lakersWithNumber = [
+    {value: 'bradley', number: 11},
+    {value: 'pope', number: 1},
+    {value: 'caruso', number: 4},
+    {value: 'cook', number: 2},
+    {value: 'cousins', number: 15},
+    {value: 'james', number: 23},
+    {value: 'AD', number: 3},
+    {value: 'green', number: 14},
+    {value: 'howard', number: 39},
+    {value: 'kuzma', number: 0},
+  ];
+
   const handleFetch = (query: string) => {
-    return lakers.filter(name => name.includes(query));
+    return lakersWithNumber.filter(name => name.value.includes(query));
+    // return lakersWithNumber.filter(name => name.value.includes(query)).map((name) => ({value: name}));
   }
 
-  const renderOption = (item: string) => {
+  const renderOption = (item: DataSourseType) => {
+    const itemWithNumber = item as DataSourseType<LakerPlayerProps>
+    // 这里需要添加一个类型断言，来传入T参数。这样他就是DataSourseType的一种，但是断言的具体类型只能是子类型：LakerPlayerProps
+    // 但是在函数声明上不能添加T参数否则会报错，说类型不匹配
     return (
-      <h2>Name: {item}</h2>
+      <>
+        <h2>Name: {itemWithNumber.value}</h2>
+        <h3>Number: {itemWithNumber.number}</h3>
+      </>
     )
   }
 
--- src/components/AutoComplete/AutoComplete.tsx
+++ src/components/AutoComplete/AutoComplete.tsx
@@ -1,10 +1,24 @@
 import React, {ChangeEvent, ReactElement, useState} from 'react';
 import Input, {InputProps} from '../Input/Input';
 
+interface DataSourceObject {
+  value: string;
+}
+
+export type DataSourseType<T = {}> = T & DataSourceObject;
+// 这里就是使用泛型来解决：不确定数据类型的方法。在不同使用场景下，通过动态的传入不同的T，即泛型，来产生不同的类型。
+// 如果没有传入T，那么他的类型就是DataSourceObject，即一个object，有一个field，key叫value
+
 interface AutoCompleteProps extends Omit<InputProps, 'onSelect'> {
-  fetchSuggestions: (p: string) => string[];
-  onSelect?: (p: string) => void;
-  renderOption?: (p: string) => ReactElement
+  fetchSuggestions: (p: string) => DataSourseType[];
+  onSelect?: (p: DataSourseType) => void;
+  renderOption?: (p: DataSourseType) => ReactElement;
+  // 当用户打算自定义 显示的dropdown的item的样式的时候，就需要用到这个prop来自定义打算如何render这些item
+  // 但是有的时候，当item的数据结构不是一个简单的string，就拿storybook中的距离：一开始数据结构是一个string[]：
+  // ['bradley', 'pope', 'caruso', 'cook', 'cousins', 'james', 'AD', 'green', 'howard', 'kuzma', 'McGee', 'rando']
+  // 但是后来如果变成了一个object[]，即object为item类型的数组，这个renderOption就不够灵活了。
+  // 而且自定义模板一般不会只展示一个简单的字符串，他会展示的每一项里的信息比较多。就好比后面的name + number
+  // 但是因为这个组件，可能接收到的数据结构并不都一样，所以这个时候就需要用到：泛型
 }
 
 export const AutoComplete: React.FC<AutoCompleteProps> = (props: AutoCompleteProps) => {
@@ -17,10 +31,10 @@ export const AutoComplete: React.FC<AutoCompleteProps> = (props: AutoCompletePro
   } = props;
 
   const [inputValue, setInputValue] = useState('');
-  const [suggestions, setSuggestions] = useState<string[]>([]);
+  const [suggestions, setSuggestions] = useState<DataSourseType[]>([]);
 
-  const renderTemplate = (item : string) => {
-    return renderOption ? renderOption(item) : item
+  const renderTemplate = (item : DataSourseType) => {
+    return renderOption ? renderOption(item) : item.value
   }
 
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
@@ -34,8 +48,8 @@ export const AutoComplete: React.FC<AutoCompleteProps> = (props: AutoCompletePro
     }
   }
 
-  const handleSelect = (item: string) => {
-    setInputValue(item);
+  const handleSelect = (item: DataSourseType) => {
+    setInputValue(item.value);
     setSuggestions([]);
     if (onSelect) {
       onSelect(item);
